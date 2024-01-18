edb6eba8959ca0ea0a0cfd55535a7599591e5afc
Sami Cheong <samicheong@gmail.com>
2024-01-18 10:16:50 -0600

    lecture2 code

--- lecture2/mlr_brain_example.R
+++ lecture2/mlr_brain_example.R
@@ -40,4 +40,32 @@ plot(fit,pch=19,which = 2)
 ## plot 4: cook's distance vs leverage
 plot(fit,pch=19,which = 6)
 
+## model evaluation criteria
+##
+fit2 <- lm(PIQ~ Brain + Height, data= data)
 
+anova(fit2,fit)
+## verify with manual RSS calculation
+## from model 1 ('fit')
+## create vector of y-yhat (since sum(y-yhat)^2 = y_diff'*y_diff)
+y1_diff <- data$PIQ - fitted(fit)
+RSS1<-t(y1_diff)%*%(y_diff)
+# number of params for model 1
+p1<-length(coef(fit))
+
+y2_diff <- data$PIQ - fitted(fit2)
+RSS2<-t(y2_diff)%*%(y2_diff)
+# number of params for model 2
+p2<-length(coef(fit2))
+
+Fstar <- round(((RSS1-RSS2)/(p1-p2))/(RSS1/(nrow(data)-p1)),3)
+
+## theoretically, Fstar should follow the F distribution 
+## with degrees of freedom: p1-p2, n-p1
+## P(F > Fstar) = 1- P(F\leq Fstar)
+p_val <- 1 - pf(Fstar,p1-p2,nrow(data)-p1)
+
+
+## model evaluation by AIC
+
+AIC(fit,fit2)
