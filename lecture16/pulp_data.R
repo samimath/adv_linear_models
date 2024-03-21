9aed51111d7088f7d4cc6965c059023731ba9a2a
Sami Cheong <samicheong@gmail.com>
2024-03-20 21:44:49 -0500

    lecture16 code on lme inference

---
+++ lecture16/pulp_data.R
@@ -0,0 +1,89 @@
+## hypothesis testing with mixed effects model - pulp data 
+library(faraway)
+library(lme4)
+library(ggplot2)
+
+## visualizing the data:
+ggplot(pulp)+
+  geom_point(aes(x=operator, 
+                 y=bright,
+                 col=operator),
+             size = 3)+theme_bw()+
+  geom_abline(slope = 0,
+              intercept = mean(pulp$bright))
+
+
+## model 0: null model:
+nullmod <- lm(bright~1, pulp)
+## model 1: mixed effects model:
+mmod <- lmer(bright ~ 1+(1|operator), pulp,
+             REML = FALSE)
+
+
+
+## compute test statistic:
+lrtstat <- as.numeric(2*(logLik(mmod)-logLik(nullmod)))
+summary(mmod)
+## compute p-value, the degrees of freedom is 1 here:
+pvalue <- pchisq(lrtstat,1,lower=FALSE)
+data.frame(lrtstat, pvalue)
+
+## for comparison, consider a regular lm 
+## that uses operator as fixed effect:
+lmod0 <- lm(bright~1, data = pulp)
+lmod <- lm(bright~operator, data = pulp)
+## computet test statistic:
+lrtstat_lm <- as.numeric(2*(logLik(lmod)-logLik(lmod0)))
+## compute p-value, the degrees of freedom is 1 here:
+pvalue_lm <- pchisq(lrtstat_lm,1,lower=FALSE)
+data.frame(lrtstat_lm, pvalue_lm)
+anova(lmod)
+
+
+## Parametric bootstrap:
+## Fit full model and null model to the data 
+
+## Compute test statistic 
+
+## Simulate pseudo-data from the null model
+y <- simulate(nullmod)
+sim_num = 1000
+lrstat <- numeric(sim_num) ## run 1000 scenarios
+set.seed(123)
+for(i in 1:sim_num){
+  y <- unlist(simulate(nullmod))
+  bnull <- lm(y ~ 1) ## null model 
+  balt <- lmer(y ~ 1 + (1|operator), 
+               pulp, REML=FALSE) ## full model
+  lrstat[i] <- as.numeric(2*(logLik(balt)-logLik(bnull))) ## test stat
+}
+## find the proportion of times the LRT returns significant results
+sig_prop <- mean(lrstat < 0.00001)
+
+
+## For curiosity, how does the result change as number of simulation changes?
+
+LRT_bootstrap<-function(sim_num){
+  
+  y <- simulate(nullmod)
+  #sim_num = 1000
+  lrstat <- numeric(sim_num) ## run 1000 scenarios
+  set.seed(123)
+  for(i in 1:sim_num){
+    y <- unlist(simulate(nullmod))
+    bnull <- lm(y ~ 1) ## null model 
+    balt <- lmer(y ~ 1 + (1|operator), 
+                 pulp, REML=FALSE) ## full model
+    lrstat[i] <- as.numeric(2*(logLik(balt)-logLik(bnull))) ## test stat
+  }
+  
+  sig_prop <- mean(lrstat < 0.00001)
+  
+return(sig_prop)
+}
+
+sim_list<-c(3,50,100,500,1000)
+sig_prop<- unlist(lapply(sim_list, function(v){LRT_bootstrap(v)}))
+plot(sim_list,sig_prop,type='b',lwd=3,col='b')
+
+
